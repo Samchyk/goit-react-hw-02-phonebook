a6437b618acf0c3bb9091abf4f1fdca07b8887df
Oleksii Samchyk <Aleksei.samchik@gmail.com>
2022-07-28 11:52:51 +0300

    ..

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,30 +3,30 @@ import ContactItem from './ContactItem/ContactItem';
 import s from './ContactList.module.css';
 
 const ContactList = ({ visibleContacts, deleteContact }) => {
-   return (
-      <ul className={s.list}>
-         {visibleContacts.map(({ id, name, number }) => (
-            <ContactItem
-               key={id}
-               id={id}
-               name={name}
-               number={number}
-               deleteContact={deleteContact}
-            />
-         ))}
-      </ul>
-   );
+  return (
+    <ul className={s.list}>
+      {visibleContacts.map(({ id, name, number }) => (
+        <ContactItem
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          deleteContact={deleteContact}
+        />
+      ))}
+    </ul>
+  );
 };
 
 ContactList.propTypes = {
-   visibleContacts: PropTypes.arrayOf(
-      PropTypes.shape({
-         id: PropTypes.string.isRequired,
-         name: PropTypes.string.isRequired,
-         number: PropTypes.string.isRequired,
-      })
-   ),
-   deleteContact: PropTypes.func.isRequired,
+  visibleContacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  deleteContact: PropTypes.func.isRequired,
 };
 
 export default ContactList;
