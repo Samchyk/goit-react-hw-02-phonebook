44abb95728f776feee865e4238556bee7750ffe3
Oleksii Samchyk <Aleksei.samchik@gmail.com>
2022-08-11 11:24:14 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,8 @@ class App extends Component {
     ],
     filter: '',
   };
+
+  
   addContact = newContact => {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
